4672efcba476a1563518992b891e4336557dea3a
vitosandrin <sandrinvito10@gmail.com>
2025-01-03 03:35:22 -0300

    wip

---
+++ backend/src/API/Extensions/HealthCheckExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace API.Extensions
+{
+    public static class HealthCheckExtensions
+    {
+        public static IServiceCollection AddDatabaseHealthCheck(this IServiceCollection services, IConfiguration configuration)
+            => services.AddHealthChecks()
+                .AddMySql(
+                    configuration.GetConnectionString("Default")!,
+                    name: "database",
+                    failureStatus: HealthStatus.Unhealthy,
+                    tags: ["db", "sql"]).Services;
+    }
+}
--- backend/src/API/Program.cs
+++ backend/src/API/Program.cs
@@ -1,13 +1,18 @@
+using API.Extensions;
+using Infrastructure.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddDatabaseHealthCheck(builder.Configuration);
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
 app.MapControllers();
+app.MapHealthChecks("/health");
 
 app.Run();
