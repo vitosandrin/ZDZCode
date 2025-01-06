d0eac046a1777d0cc856d33f4100d1e2205c908b
vitosandrin <sandrinvito10@gmail.com>
2025-01-06 02:55:19 -0300

    feat: Exntesions  MediaTR

---
+++ backend/src/API/Extensions/MediatRExtensions.cs
@@ -0,0 +1,20 @@
+﻿using API.Behaviours;
+using FluentValidation;
+using Infrastructure.Exceptions;
+using System.Reflection;
+
+namespace API.Extensions
+{
+    public static class MediatRExtensions
+    {
+        public static IServiceCollection AddMediatR(this IServiceCollection services)
+            => services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssemblies(Assembly.Load("Application"));
+                cfg.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+                cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            })
+            .AddValidatorsFromAssembly(Assembly.Load("Application"))
+            .AddExceptionHandler<CustomExceptionHandler>();
+    }
+}
