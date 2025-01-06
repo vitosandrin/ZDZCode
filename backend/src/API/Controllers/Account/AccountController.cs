f7005d2f3810b9f2732c65f774fd2a1fb7c317f1
vitosandrin <sandrinvito10@gmail.com>
2025-01-06 02:54:56 -0300

    feat: account controller

---
+++ backend/src/API/Controllers/Account/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Application.UseCases.Commands;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
+namespace API.Controllers.Account
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController(IMediator mediator) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator;
+
+        [HttpPost]
+        [Route("create")]
+        public async Task<IActionResult> CreateAccount([FromBody] Request.CreateAccount request)
+        {
+            var command = new CreateAccountCommand(request.AccountNumber, request.InitialDeposit);
+
+            var accountId = await _mediator.Send(command);
+            return Ok(new { AccountId = accountId });
+        }
+    }
+}
---
+++ backend/src/API/Controllers/Account/Requests.cs
@@ -0,0 +1,7 @@
+﻿namespace API.Controllers.Account
+{
+    public static class Request
+    {
+        public sealed record CreateAccount(string AccountNumber, decimal InitialDeposit);
+    }
+}
