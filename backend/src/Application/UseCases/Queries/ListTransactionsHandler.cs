ae41598f2be5eef34c1ae4547208ff72915384b0
vitosandrin <sandrinvito10@gmail.com>
2025-01-06 02:57:17 -0300

    feat: ListTransactionsQuery

---
+++ backend/src/Application/UseCases/Queries/ListTransactionsHandler.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Aggregates;
+using Infrastructure.Exceptions;
+using Infrastructure.Repositories;
+using MediatR;
+
+namespace Application.UseCases.Queries
+{
+    public sealed record ListTransactionsQuery(Guid AccountId) : IRequest<IEnumerable<Transaction>>;
+
+    public class ListTransactionsHandler(IUnityOfWork unityOfWork) : IRequestHandler<ListTransactionsQuery, IEnumerable<Transaction>>
+    {
+        private readonly ITransactionRepository _transactionRepository = unityOfWork.TransactionRepository;
+        private readonly IAccountRepository _accountRepository = unityOfWork.AccountRepository;
+
+        public async Task<IEnumerable<Transaction>> Handle(ListTransactionsQuery query, CancellationToken cancellationToken)
+        {
+            var _ = await _accountRepository.GetByIdAsync(query.AccountId)
+                ?? throw new NotFoundException("Account", query.AccountId);
+
+            var transactions = await _transactionRepository.GetByAccountIdAsync(query.AccountId);
+
+            return transactions;
+        }
+    }
+}
